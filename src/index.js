91fa52cb4e9b3e59c560f9cba45d78f203d4c07b
Likvidas <bobento@yandex.ru>
2020-03-10 10:37:05 +0300

    init: start Reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n * -1;
+    }
+
+    let numbToStr = String(n);
+    let reverseN = '';
+
+    for (let i = numbToStr.length - 1; i >= 0; i--) {
+        reverseN += numbToStr[i];
+    }
+
+    return parseInt(reverseN);
 }
